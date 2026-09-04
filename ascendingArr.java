a644c33c6e86992348d7633da5fbdfdf6e64f4f9
Zaid Ajmeri <111185630+zaidajmeri@users.noreply.github.com>
2026-09-04 21:17:36 +0530

    Add ascending array sorting program
    Implemented a Java program to sort an array in ascending order and print the sorted array.

---
+++ ascendingArr.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class ascendingArr {
+	public static void main(String[] args) {
+		int[] arr = { 50, 20, 40, 10, 30 };
+
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] > arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+
+				}
+			}
+
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
